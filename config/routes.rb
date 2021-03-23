d8697f6b1973bec3e6434bf9977daa65d795355c
jacobperia <jacob.peria@hotmail.com>
2021-03-23 12:37:31 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  get "about-us", to:"about#index", as: :about
+  root to:"main#index"
+  # OR USE, get "/", to:"main#index"
+end
